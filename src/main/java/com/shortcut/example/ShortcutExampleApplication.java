f9c9464125a45755a321dbcb455cb14c57ef3ccc
sergeilisovoi22 <sergeilisovoi22@gmail.com>
2025-01-17 16:19:37 +0300

    initial commit

---
+++ src/main/java/com/shortcut/example/ShortcutExampleApplication.java
@@ -0,0 +1,13 @@
+package com.shortcut.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortcutExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortcutExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shortcut/example/ShortcutExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shortcut.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortcutExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
